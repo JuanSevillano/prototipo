883f53efd72a1de18ed089327aa54d9dfdf6dd53
Juan Sevillano <sevi0_6@hotmail.com>
2017-08-28 22:47:04 -0500

    background gradient and starting fragments (login, resgister)

--- app/src/main/java/com/example/sevillano/proto_2/Inicio.java
+++ app/src/main/java/com/example/sevillano/proto_2/Inicio.java
@@ -1,7 +1,9 @@
 package com.example.sevillano.proto_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +16,13 @@ public class Inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
         continuar = (TextView) findViewById(R.id.continuar);
+    }
+
+    public void explorar(View v){
+        Intent i = new Intent(this,MainActivity.class);
+        String location = continuar.toString();
+        i.putExtra("ubicacion",location);
+        startActivity(i);
     }
 
 
